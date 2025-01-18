c9aef72b80e56d1deb24ea905d93cec39eb5f0c6
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2025-01-18 13:00:11 +0100

    Initial Commit

---
+++ EasyScreenshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyScreenshot
+//
+//  Created by Bolaji Olajide on 18/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyScreenshot/EasyScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenshotApp.swift
+//  EasyScreenshot
+//
+//  Created by Bolaji Olajide on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
